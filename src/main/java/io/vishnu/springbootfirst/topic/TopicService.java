baff536ac4769c953f6e05cdac447b5d0f07a639
Vishnu <vishnu.c1@emc.com>
2021-05-09 12:47:40 +0530

    Initital commit of course-api

---
+++ src/main/java/io/vishnu/springbootfirst/topic/TopicService.java
@@ -0,0 +1,29 @@
+package io.vishnu.springbootfirst.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
+			new Topic("spring", "spring framework", "description1"),
+			new Topic("java", "java framework", "description2")
+			));
+	
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+	
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+}
